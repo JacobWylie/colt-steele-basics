fcb39a988e391495f5ba9f19440fedce1aae689c
JacobWylie <jacobwylie@gmail.com>
2017-05-30 15:21:59 +0200

    annoy-o-matic

--- loop-problems.js
+++ loop-problems.js
@@ -1,3 +1,6 @@
+///////////////////////
+// PROBLEM ONE
+///////////////////////
 const problemOne = document.querySelector('.one');
 for (i=-10;i<=19;i++) {
   let iDiv = document.createElement('button');
@@ -8,6 +11,9 @@ for (i=-10;i<=19;i++) {
   problemOne.appendChild(iDiv);
 }
 
+///////////////////////
+// PROBLEM TWO
+///////////////////////
 const problemTwo = document.querySelector('.two');
 for (i=10;i<=40;i+=2) {
   let iDiv = document.createElement('button');
@@ -18,6 +24,9 @@ for (i=10;i<=40;i+=2) {
   problemTwo.appendChild(iDiv);
 }
 
+///////////////////////
+// PROBLEM THREE
+///////////////////////
 const problemThree = document.querySelector('.three');
 for (i=333;i>=300;i-=2) {
   let iDiv = document.createElement('button');
@@ -28,6 +37,9 @@ for (i=333;i>=300;i-=2) {
   problemThree.appendChild(iDiv);
 }
 
+///////////////////////
+// PROBLEM FOUR
+///////////////////////
 const problemFour = document.querySelector('.four');
 for (i=5;i<=50;i++) {
   if(i%15===0) {
@@ -39,3 +51,49 @@ for (i=5;i<=50;i++) {
     problemFour.appendChild(iDiv); 
   }
 }
+
+
+///////////////////////
+// PROBLEM FIVE
+///////////////////////
+const problemFive = document.querySelector('.five');
+
+const annoyomatic = () => {
+	let userInput = prompt("Are we there yet?").toLowerCase();
+	if (userInput === 'yeah' || userInput === 'yes') {
+		let iDiv = document.createElement('h5')
+		iDiv.innerHTML = `<br>Yay, we finally made it!`;
+    	problemFive.appendChild(iDiv);
+	} else {
+		annoyomatic();
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
