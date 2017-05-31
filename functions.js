08e8c3ba2ce1c48c284a3b784e9a0c597185faa2
JacobWylie <jacobwylie@gmail.com>
2017-05-31 15:54:48 +0200

    function junctions

--- functions.js
+++ functions.js
@@ -1,5 +1,65 @@
-const sayHello = name => console.log(`Hello ${name}`);
-sayHello('jacob');
+const isEven = num => num % 2 === 0;
+const printEven = num => console.log(isEven(num));
+printEven(4);
+printEven(21);
+printEven(68);
+printEven(333);
 
-const square = num => num * num;
-console.log(square(10 * 2));
+const factorial = num => {
+	if(num === 0) {
+		return 1;
+	}
+	return num * factorial(num-1)
+}
+const printFact = (num) => console.log(factorial(num));
+printFact(5);
+printFact(2);
+printFact(10);
+printFact(0);
+
+const kebabToSnake = (str) => str.replace (/\-/g, "_");
+const printKebab = (kebab) => console.log(kebabToSnake(kebab))
+printKebab("hello-world");
+printKebab("dogs-are-awesome");
+printKebab("blah");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
