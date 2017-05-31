574ebc8d127a6ec1d6e76911f13ae1f2d8dc945e
JacobWylie <jacobwylie@gmail.com>
2017-05-31 19:25:53 +0200

    refactored array todo list

--- arrays2.js
+++ arrays2.js
@@ -1,15 +1,37 @@
 let todos = [];
 
+const listTodos = () => {
+	console.log(`*****************`);
+	todos.forEach((todo, index) => {
+		console.log(`${index}: ${todo}`)
+	})
+	console.log(`*****************`);
+	todoList();
+}
+
+const newTodos = () => {
+	todos.push(prompt("add item"));
+	console.log(`Added Todo`)
+	todoList();
+}
+
+const deleteTodos = () => {
+	let indexDelete = prompt("Enter index of item to delete");
+	todos.splice(indexDelete, 1);
+	console.log(`Deleted Todo`)
+	todoList()
+}
+
 const todoList = () => {
 	let input = prompt("what would you like to do");
 	if(input === "list") {
-		console.log(todos);
-		todoList();
+		listTodos();
 	} else if (input === "new") {
-		todos.push(prompt("add item"));
-		todoList();
+		newTodos();
 	} else if (input === "quit") {
 		alert('have a good one');
+	} else if (input === "delete") {
+		deleteTodos();
 	} else {
 		todoList();
 	}	
---
+++ arrays3.js
@@ -0,0 +1,8 @@
+// For Each
+
+arr.forEach(someFunction)
+
+const printColor = color => console.log(color);
+var colors = ['red', 'orange', 'yellow', 'green']
+
+colors.forEach(printColor);
