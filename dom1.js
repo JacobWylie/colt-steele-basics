a42e212902e44c8aa25425df36f08116ee666847
JacobWylie <jacobwylie@gmail.com>
2017-06-06 17:29:00 +0200

    dom1 done

--- dom1.js
+++ dom1.js
@@ -3,12 +3,14 @@ const h2 = document.querySelector('h2');
 const highlight = document.querySelector('#highlight');
 const bold = document.querySelectorAll('.bolded');
 
+h1.addEventListener('mouseover', () => h1.style.cursor = 'help');
 h1.addEventListener('click', () => {
 	window.location = 'http://www.google.com';
 })
 
 window.addEventListener('mousemove', () => {
 	h2.style.color = '#'+Math.random().toString(16).substr(-6);
+	document.body.style.cursor = 'crosshair';
 })
 
 highlight.addEventListener('mouseover', () => highlight.style.color = 'pink');
@@ -22,4 +24,5 @@ bold.forEach( (bold) => {
 			bold.style.fontWeight = 'bold';
 		}
 	})
+	bold.addEventListener('mouseover', () => bold.style.cursor = 'progress');
 })
