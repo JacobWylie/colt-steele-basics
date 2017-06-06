774bd437a5df1f858c27f4d880470a292b176007
JacobWylie <jacobwylie@gmail.com>
2017-06-06 18:55:31 +0200

    dom manipulated

--- dom1.js
+++ dom1.js
@@ -4,17 +4,16 @@ const highlight = document.querySelector('#highlight');
 const bold = document.querySelectorAll('.bolded');
 
 h1.addEventListener('mouseover', () => h1.style.cursor = 'help');
-h1.addEventListener('click', () => {
-	window.location = 'http://www.google.com';
-})
+h1.addEventListener('click', () => h1.innerHTML = 'Sweet')
 
 window.addEventListener('mousemove', () => {
 	h2.style.color = '#'+Math.random().toString(16).substr(-6);
 	document.body.style.cursor = 'crosshair';
 })
 
-highlight.addEventListener('mouseenter', () => highlight.innerHTML = "<img src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/512px-Unofficial_JavaScript_logo_2.svg.png'></img>");
-// highlight.addEventListener('mouseout', () => highlight.innerHTML = 'List Item 1 (mouseover)');
+highlight.addEventListener('mouseenter', () => highlight.setAttribute('src', 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/512px-Unofficial_JavaScript_logo_2.svg.png'));
+const logo = document.querySelector('#logo');
+highlight.addEventListener('mouseout', () => highlight.setAttribute('src', 'https://www.brandsoftheworld.com/sites/default/files/styles/logo-original-577x577/public/082014/js1_0.png?itok=zdiqYk3L'));
 
 
 
