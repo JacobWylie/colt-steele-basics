0464d50ec95d65528b853e1d5beecefa67c8ea1b
JacobWylie <jacobwylie@gmail.com>
2017-06-07 16:43:40 +0200

    messing with google.com

---
+++ dom2.js
@@ -0,0 +1,18 @@
+const googleLogo = document.querySelector('div[title="google"]');
+const input = document.querySelector('input[value="Google Search"]');
+const subtext = document.querySelector('.logo-subtext');
+
+googleLogo.style.background = 'url("http://logos-download.com/wp-content/uploads/2016/02/bing-tm-logo.png")';
+
+input.value = "Feeling lucky punk?";
+
+subtext.innerHTML = 'Welcome to Bing!';
+
+
+
+
+
+
+
+
+
