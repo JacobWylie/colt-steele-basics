c4e967bcdeee64a5d9cf4c2a01e8a496e4ec1da7
JacobWylie <jacobwylie@gmail.com>
2017-06-09 14:06:42 +0200

    refactored functions

--- dom4.js
+++ dom4.js
@@ -1,7 +1,8 @@
-const playerOneScore = document.querySelector('#playerOneScore');
+const playerOneScore = document.querySelector(`#playerOneScore`);
 let playerOneCounter = 0;
 const playerTwoScore = document.querySelector('#playerTwoScore');
 let playerTwoCounter = 0;
+let winner;
 const winnerBanner = document.querySelector('#winnerBanner');
 const scoreLimitDisplay = document.querySelector('#scoreLimitDisplay');
 const scoreLimit = document.querySelector('input');
@@ -9,57 +10,36 @@ const playerOneButton = document.querySelector('#playerOneButton');
 const playerTwoButton = document.querySelector('#playerTwoButton');
 const resetButton = document.querySelector('#resetButton');
 
-playerOneButton.addEventListener('click', () => {
-	if (playerOneCounter < scoreLimit.value) {
-		playerOneCounter++;
-		playerOneScore.innerHTML = playerOneCounter;
-		if (scoreLimit.value >= 1 && playerOneCounter == scoreLimit.value) {
-			playerOneWins(playerOneCounter);
-		}
-	}
-})
-
-playerTwoButton.addEventListener('click', () => {
-	if (playerTwoCounter < scoreLimit.value) {
-		playerTwoCounter++;
-		playerTwoScore.innerHTML = playerTwoCounter;
-		if (scoreLimit.value >= 1 && playerTwoCounter == scoreLimit.value) {
-			playerTwoWins(playerTwoCounter);
-		}
-	}
-})
-
-const playerOneWins = playerOneCounter => {
+const playerOneWins = () => {
 		playerOneScore.style.color = 'green';
-		winnerBanner.innerHTML = "Player One Wins!";
-		setInterval( () => {
-	    	winnerBanner.style.color = '#'+Math.random().toString(16).substr(-6);
-	    }, 300);
-		playerOneButton.disabled = true;
-		playerTwoButton.disabled = true;
+		winnerDisplay();
+		disableButtons();
 }
 
-const playerTwoWins = playerTwoCounter => {
+const playerTwoWins = () => {
 		playerTwoScore.style.color = 'green';
-		winnerBanner.innerHTML = "Player Two Wins!";
-		setInterval( () => {
-	    	winnerBanner.style.color = '#'+Math.random().toString(16).substr(-6);
-	    }, 300);
-		playerOneButton.disabled = true;
-		playerTwoButton.disabled = true;
+		winnerDisplay();
+		disableButtons();
 }
 
-resetButton.addEventListener('click', () => {
-	playerOneCounter = 0; 
-	playerTwoCounter = 0;
-	playerOneScore.innerHTML = playerOneCounter;
-	playerTwoScore.innerHTML = playerTwoCounter;
-	winnerBanner.innerHTML = '';
-	playerOneScore.style.color = 'black';
-	playerTwoScore.style.color = 'black';
+const winnerDisplay = () => {
+	winnerBanner.innerHTML = `Player ${winner} Wins!`;
+	setInterval( () => {
+	    	winnerBanner.style.color = '#'+Math.random().toString(16).substr(-6);
+	    }, 300);
+}
+
+const disableButtons = () => {
+	playerOneButton.disabled = true;
+	playerTwoButton.disabled = true;
+}
+
+const enableButtons = () => {
 	playerOneButton.disabled = false;
 	playerTwoButton.disabled = false;
-})
+}
+
+// BUTTON FUNCTIONALITY
 
 scoreLimit.addEventListener('input', () => {
 	if (scoreLimit.value >= 0) {
@@ -69,6 +49,39 @@ scoreLimit.addEventListener('input', () => {
 			scoreLimitDisplay.innerHTML = scoreLimit.value;
 		}
 	}
-
 })
+
+playerOneButton.addEventListener('click', () => {
+	if (playerOneCounter < scoreLimit.value) {
+		playerOneCounter++;
+		playerOneScore.innerHTML = playerOneCounter;
+		if (scoreLimit.value >= 1 && playerOneCounter == scoreLimit.value) {
+			winner = "One";
+			playerOneWins();
+		}
+	}
+})
+
+playerTwoButton.addEventListener('click', () => {
+	if (playerTwoCounter < scoreLimit.value) {
+		playerTwoCounter++;
+		playerTwoScore.innerHTML = playerTwoCounter;
+		if (scoreLimit.value >= 1 && playerTwoCounter == scoreLimit.value) {
+			winner = "Two"
+			playerTwoWins();
+		}
+	}
+})
+
+resetButton.addEventListener('click', () => {
+	playerOneCounter = 0; 
+	playerTwoCounter = 0;
+	playerOneScore.innerHTML = playerOneCounter;
+	playerTwoScore.innerHTML = playerTwoCounter;
+	winnerBanner.innerHTML = '';
+	playerOneScore.style.color = 'black';
+	playerTwoScore.style.color = 'black';
+	enableButtons();
+})
+
 
