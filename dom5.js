2403d901ef0842176443b64868c35c4d41362f93
JacobWylie <jacobwylie@gmail.com>
2017-06-09 17:04:25 +0200

    add item to list

--- dom5.js
+++ dom5.js
@@ -1,5 +1,29 @@
 const todo = document.querySelectorAll('.todo');
+const list = document.querySelector('ul')
+const input = document.querySelector('input');
+const button = document.querySelector('button');
 
+button.addEventListener('click', () => {
+	if (input.value.length !== 0) {
+		const newItem = document.createElement('li');
+		newItem.addEventListener('mouseover', function() {
+	    	this.classList.toggle('mouseOver');
+	    })
+	    newItem.addEventListener('mouseout', function() {
+	    	this.classList.toggle('mouseOver');
+		})
+	    newItem.addEventListener('click', function() {
+	    	this.classList.toggle('strikeout');
+		})
+		newItem.innerHTML = input.value;
+		list.appendChild(newItem);
+		input.value = "";
+	}
+})
+
+
+
+// For Example todo items already in list
 todo.forEach( todo => {
   todo.addEventListener('mouseover', function() {
     this.classList.toggle('mouseOver');
@@ -10,4 +34,4 @@ todo.forEach( todo => {
   todo.addEventListener('click', function() {
     this.classList.toggle('strikeout');
   })
-});
+})
