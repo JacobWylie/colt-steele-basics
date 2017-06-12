4b3cce559dfb3cdad494d6d3eff9c9d8aa5179fc
JacobWylie <jacobwylie@gmail.com>
2017-06-12 18:43:27 +0200

    create colors array from function

--- color-game.js
+++ color-game.js
@@ -1,21 +1,33 @@
 const squares = document.querySelectorAll('.square');
-const rgbDisplay = document.querySelector('.rgbDisplay')
+const rgbDisplay = document.querySelector('.rgbDisplay');
+const notice = document.querySelector('.notice');
+const newColors = document.querySelector('.newColors');
+const header = document.querySelector('.header');
+const footer = document.querySelector('.navbar');
 
 const randomNumber = () => Math.floor(Math.random()*256).toString();
 const randomRGB = () => `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`;
-let colors = [
-	randomRGB(),
-	randomRGB(),
-	randomRGB(),
-	randomRGB(),
-	randomRGB(),
-	randomRGB(),
-]
 
-const winningColor = colors[Math.floor(Math.random()*colors.length)];
+const generateRandomColors = (num) => {
+	// make an array
+	let arr = []
+	// add num random colors to array
+	for(i = 0; i < num; i++) {
+		arr.push(randomRGB());
+	}
+	// return array
+	return arr;
+}
 
+let colors = generateRandomColors(6);
+
+// select winning color from array
+const winningColor = colors[Math.floor(Math.random() * colors.length)];
+
+//display winning color rgb value in header
 rgbDisplay.textContent = winningColor;
 
+// loop through color squares
 for(i = 0; i < squares.length; i++) {
 	// add initial colors to squares
 	squares[i].style.backgroundColor = colors[i];
@@ -26,8 +38,16 @@ for(i = 0; i < squares.length; i++) {
 		let clickedColor = this.style.backgroundColor;
 		// compare color to winningColor
 		if (clickedColor === winningColor) {
-			alert('goodjob');
+			notice.textContent = 'GREAT JOB!';
+			newColors.textContent = 'TRY AGAIN?';
+			header.style.backgroundColor = winningColor;
+			footer.style.backgroundColor = winningColor;
+			squares.forEach(squares => {
+				squares.style.backgroundColor = winningColor;
+				squares.classList.remove('hideSquare');
+			});
 		} else {
+			notice.textContent = "TRY AGAIN!"
 			this.classList.add('hideSquare');
 		}
 	})	
