269457d5df28fc62c0b369f794eab92aaefe1c13
JacobWylie <jacobwylie@gmail.com>
2017-06-12 17:36:30 +0200

    functionality to guessing game

--- color-game.js
+++ color-game.js
@@ -1,6 +1,36 @@
 const squares = document.querySelectorAll('.square');
+const rgbDisplay = document.querySelector('.rgbDisplay')
 
-squares.forEach(squares => {
-	squares.style.backgroundColor = '#'+Math.random().toString(16).substr(-6);
-})
+const randomNumber = () => Math.floor(Math.random()*256).toString();
+const randomRGB = () => `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`;
+let colors = [
+	randomRGB(),
+	randomRGB(),
+	randomRGB(),
+	randomRGB(),
+	randomRGB(),
+	randomRGB(),
+]
+
+const winningColor = colors[Math.floor(Math.random()*colors.length)];
+
+rgbDisplay.textContent = winningColor;
+
+for(i = 0; i < squares.length; i++) {
+	// add initial colors to squares
+	squares[i].style.backgroundColor = colors[i];
+
+	//add click listeners to squares
+	squares[i].addEventListener('click', function() {
+		// grab color of clicked square
+		let clickedColor = this.style.backgroundColor;
+		// compare color to winningColor
+		if (clickedColor === winningColor) {
+			alert('goodjob');
+		} else {
+			this.classList.add('hideSquare');
+		}
+	})	
+}
+
 
