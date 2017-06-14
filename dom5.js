2e7ff6d20b8b7412f5c44dae04774718e3ce059c
JacobWylie <jacobwylie@gmail.com>
2017-06-14 16:15:34 +0200

    fixed todo list delete functionality

--- dom5.js
+++ dom5.js
@@ -1,26 +1,38 @@
 const todo = document.querySelectorAll('.todo');
 const list = document.querySelector('ul')
 const input = document.querySelector('input');
-const button = document.querySelector('button');
+const addButton = document.querySelector('.addButton');
 
-button.addEventListener('click', () => {
+//deletes list item
+function deleter () {
+  let deleteButton = document.querySelectorAll('.close');
+  deleteButton.forEach( deleteButton => {
+    deleteButton.addEventListener('click', function() {
+     this.parentNode.remove();
+    })
+  })
+}
+
+// Creates new todo list items
+addButton.addEventListener('click', () => {
 	if (input.value.length !== 0) {
 		const newItem = document.createElement('li');
 		newItem.addEventListener('mouseover', function() {
-	    	this.classList.toggle('mouseOver');
-	    })
-	    newItem.addEventListener('mouseout', function() {
-	    	this.classList.toggle('mouseOver');
+	    this.classList.toggle('mouseOver');
+	  })
+	  newItem.addEventListener('mouseout', function() {
+	    this.classList.toggle('mouseOver');
 		})
-	    newItem.addEventListener('click', function() {
-	    	this.classList.toggle('strikeout');
+	  newItem.addEventListener('click', function() {
+	    this.classList.toggle('strikeout');
 		})
-		newItem.innerHTML = input.value;
+    newItem.classList.add('todo');
+		newItem.innerHTML = `<span class='close'></span>${input.value}`;
 		list.appendChild(newItem);
 		input.value = "";
 	}
+  deleter();
 })
-
 
 
 // For Example todo items already in list
@@ -34,4 +46,10 @@ todo.forEach( todo => {
   todo.addEventListener('click', function() {
     this.classList.toggle('strikeout');
   })
+  todo.addEventListener('click', deleter() );
 })
+
+
+
+
+
