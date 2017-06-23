0ad69eac10040044a61cf6adc8c89272a876d586
JacobWylie <jacobwylie@gmail.com>
2017-06-23 17:12:06 +0200

    add to list

--- colts-todo/assets/js/jquery2.js
+++ colts-todo/assets/js/jquery2.js
@@ -1,9 +1,20 @@
 // Check off todo items when clicked
-$('li').click(function () {
+$('ul').on('click', 'li', function () {
 	$(this).toggleClass('check-off');
 })
 
-// Delete todo item
-$('.delete').click(function() {
-	$(this).parent().remove();
+// Click X to delete todo item
+$('ul').on('click', '.delete', function(e) {
+	e.stopPropagation();
+	$(this).parent().fadeOut(500, function() {
+		$(this).remove();
+	});
+})
+
+// Add item to Todo list
+$('input[type="text"]').keypress(function(e) {
+	if(e.which === 13) {
+		$('ul').append(`<li><span class='delete'>X</span> ${$(this).val()}</li>`)
+		$(this).val('');
+	}
 })
