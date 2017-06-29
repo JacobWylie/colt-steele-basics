ab186e1d1b3c332e642a0aece844d8aa2fd14108
JacobWylie <jacobwylie@gmail.com>
2017-06-29 18:49:03 +0200

    built express app, receives data from OIMDB app

---
+++ express/MovieApiApp/app.js
@@ -0,0 +1,34 @@
+const express = require('express'),
+	  app = express(),
+	  request = require('request');
+app.set('view engine', 'ejs');
+
+// Home Page
+app.get('/', (req, res) => {
+	res.render('index');
+});
+
+
+// API Results
+app.get('/results', (req, res) => {
+	request('http://omdbapi.com/?s=michigan&apikey=thewdb', function (error, response, body) {
+	if(!error && response.statusCode === 200) {
+		let movie = JSON.parse(body); 
+		res.send(movie.Search[0].Title);
+	} else {
+		// Error Page
+		res.redirect('error')
+	}
+});
+})
+
+
+// All Other Requests go to error page
+app.get('*', (req, res) => {
+	res.render('error')
+})
+
+// Local Dev Server
+app.listen(3000, () => {
+	console.log('App running on port: 3000');
+});
