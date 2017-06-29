c187bcead1b400f53160e558ee06dee1abfbf36f
JacobWylie <jacobwylie@gmail.com>
2017-06-29 15:00:35 +0200

    ejs partials

--- express/routing-ejs/app.js
+++ express/routing-ejs/app.js
@@ -1,14 +1,26 @@
 const express = require('express'),
 	  app = express();
 
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
 
 app.get('/', (req, res) => {
-	res.render('index.ejs');
+	res.render('index');
 })
 
 app.get('/news/:article', (req, res) => {
 	let story = req.params.article;
-	res.render(`news/story.ejs`, {article: story})
+	res.render(`news/story`, {article: story})
+})
+
+app.get('/posts', (req, res) => {
+	let posts = [
+		{title: 'Post 1', author: 'Jimbo'},
+		{title: 'Blog Post', author: 'Jamie'},
+		{title: 'New News', author: 'Sam'}
+	]
+	res.render('posts/post', {posts: posts})
 })
 
 app.listen(3000, () => {
